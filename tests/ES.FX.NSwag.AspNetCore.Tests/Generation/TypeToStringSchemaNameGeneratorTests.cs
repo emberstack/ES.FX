9e8e0d4046cdf635d0ca9862c2956234f8f291a4
Cristian Serban <cristian.serban@selfpay.ro>
2024-07-26 17:06:52 +0300

    more unit tests and some changes

---
+++ tests/ES.FX.NSwag.AspNetCore.Tests/Generation/TypeToStringSchemaNameGeneratorTests.cs
@@ -0,0 +1,20 @@
+﻿using ES.FX.NSwag.AspNetCore.Generation;
+
+namespace ES.FX.NSwag.AspNetCore.Tests.Generation
+{
+    public class TypeToStringSchemaNameGeneratorTests
+    {
+        [Theory]
+        [InlineData(typeof(string), "System.String")]
+        [InlineData(typeof(int), "System.Int32")]
+        [InlineData(typeof(int?), "System.Nullable`1[System.Int32]")]
+        [InlineData(typeof(int[]), "System.Int32[]")]
+        [InlineData(typeof(int[,]), "System.Int32[,]")]
+        [InlineData(typeof(int[][]), "System.Int32[][]")]
+        public void TestSchemaNameGeneration(Type type, string typeName)
+        {
+            Assert.Equal(new TypeToStringSchemaNameGenerator().Generate(type), typeName);
+            
+        }
+    }
+}
