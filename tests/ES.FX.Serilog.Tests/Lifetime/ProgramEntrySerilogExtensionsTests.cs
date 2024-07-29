456ef79e711b0649163a0b325512557c56974d4c
Cristian Serban <cristian.serban@selfpay.ro>
2024-07-29 11:17:21 +0300

    update nugets and 1 unit test testcontainers fix for mssql container is still not merged yet

---
+++ tests/ES.FX.Serilog.Tests/Lifetime/ProgramEntrySerilogExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿using ES.FX.Hosting.Lifetime;
+using ES.FX.Serilog.Lifetime;
+using Microsoft.Extensions.Hosting;
+using Moq;
+using Serilog;
+using Serilog.Events;
+
+namespace ES.FX.Serilog.Tests.Lifetime
+{
+    public class ProgramEntrySerilogExtensionsTests
+    {
+        [Fact]
+        public void VerifyLoggerConfiguration()
+        {
+            var builder = Host.CreateEmptyApplicationBuilder(null);
+            var funcMock = new Mock<Action<LoggerConfiguration>>();
+
+            ProgramEntry.CreateBuilder([]).UseSerilog(LogEventLevel.Debug, funcMock.Object);
+
+            funcMock.Verify(funcMock => funcMock(It.IsAny<LoggerConfiguration>()), Times.Once);
+        }
+    }
+}
