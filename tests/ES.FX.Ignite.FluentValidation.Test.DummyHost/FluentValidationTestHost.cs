8db715bffaa6d8ff00f6be047f65274c24abbc73
Cristian Serban <cristian.serban@selfpay.ro>
2024-08-01 16:17:30 +0300

    finally testing the fluentvalidation properly created another project that is used as host

---
+++ tests/ES.FX.Ignite.FluentValidation.Test.DummyHost/FluentValidationTestHost.cs
@@ -0,0 +1,45 @@
+using FluentValidation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using SharpGrip.FluentValidation.AutoValidation.Endpoints.Extensions;
+
+internal sealed class FluentValidationTestHost
+{
+    internal static string APIRoute = "/test/autoValidation";
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddScoped<IValidator<TestRequest>, TestValidator>();
+
+        var app = builder.Build();
+
+        app.MapGet("/", async context =>
+        {
+            await context.Response.WriteAsync("hello");
+        });
+        app.MapPost(APIRoute, (TestRequest request) => { return Results.Ok(request); })
+        .AddFluentValidationAutoValidation()
+        .WithName("TestAutoValidation");
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
+
+public record TestRequest(string Name);
+
+
+internal class TestValidator : AbstractValidator<TestRequest>
+{
+    public TestValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty();
+    }
+}
---
+++ tests/ES.FX.Ignite.FluentValidation.Tests/Hosting/FluentValidationFunctionalTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Text;
+
+namespace ES.FX.Ignite.FluentValidation.Tests.Hosting
+{
+    public class FluentValidationFunctionalTests
+    {
+        [Theory]
+        [InlineData("", true)]
+        [InlineData("test", false)]
+        public async Task FluentValidation_CheckAutoValidations(string name, bool shouldThrowValidationError)
+        {
+            var client = GetClient();
+
+            var person = new TestRequest(name);
+
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(person);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync(FluentValidationTestHost.APIRoute, data);
+            string resultContent = await response.Content.ReadAsStringAsync();
+
+            Assert.True(resultContent.Contains("validation error") == shouldThrowValidationError);
+        }
+
+        private HttpClient GetClient()
+        {
+            var application = new WebApplicationFactory<FluentValidationTestHost>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        services.AddAuthorization();
+                        services.AddControllers();
+                    });
+                });
+
+            var client = application.CreateClient();
+            return client;
+        }
+    }
+}
