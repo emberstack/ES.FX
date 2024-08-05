bb6e0534df73d108f6f9e80b4e9ff7d975dd9b73
Cristian Serban <cristian.serban@selfpay.ro>
2024-08-05 16:58:57 +0300

    add nswag tests

---
+++ tests/ES.FX.Ignite.NSwag.Tests.SUT/Program.cs
@@ -0,0 +1,13 @@
+﻿using ES.FX.Ignite.NSwag.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+app.IgniteNSwag();
+
+app.Run();
+
+// ReSharper disable once UnusedMember.Global
+public partial class Program
+{
+}
---
+++ tests/ES.FX.Ignite.NSwag.Tests/Functional/NSwagFunctionalTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace ES.FX.Ignite.NSwag.Tests.Hosting
+{
+
+    public class NSwagFunctionalTests(WebApplicationFactory<Program> factory)
+        : IClassFixture<WebApplicationFactory<Program>>
+    {
+        [Fact]
+        public async Task Swagger_Accessible()
+        {
+            var client = factory.CreateClient();
+
+            var response = await client.GetAsync(
+                "/swagger/");
+            Assert.True(response.IsSuccessStatusCode);
+        }
+    }
+}
