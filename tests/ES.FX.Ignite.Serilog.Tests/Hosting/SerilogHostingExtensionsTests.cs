0159faa99c0aed8ffb457c6033ab00b4361de10a
Cristian Serban <cristian.serban@selfpay.ro>
2024-08-06 15:39:24 +0300

    add ignite serilog tests

---
+++ tests/ES.FX.Ignite.Serilog.Tests/Hosting/SerilogHostingExtensionsTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using ES.FX.Ignite.Serilog.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using ES.FX.Ignite.Spark.Exceptions;
+
+namespace ES.FX.Ignite.Serilog.Tests.Hosting
+{
+    public class SerilogHostingExtensionsTests
+    {
+        [Fact]
+        public void CanIgnite()
+        {
+            var builder = WebApplication.CreateBuilder([]);
+
+            builder.IgniteSerilog();
+
+            var serviceProvider = builder.Build().Services;
+            Assert.NotNull(serviceProvider.GetRequiredService<ILogger>());
+        }
+
+
+        [Fact]
+        public void CanIgnite_Once()
+        {
+            var builder = WebApplication.CreateBuilder([]);
+
+            builder.IgniteSerilog();
+
+            Assert.Throws<ReconfigurationNotSupportedException>(() => builder.IgniteSerilog());
+        }
+    }
+}
