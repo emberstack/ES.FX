cf6573a677047ccfb2b06781a0faa0e8698f0ab0
Cristian Serban <cristian.serban@selfpay.ro>
2024-08-07 11:37:29 +0300

    add seq fixture container and seq functional tests

---
+++ tests/ES.FX.Ignite.OpenTelemetry.Exporter.Seq.Tests/FunctionalTests.cs
@@ -0,0 +1,55 @@
+﻿using ES.FX.Shared.Seq.Tests.Fixtures;
+using ES.FX.Ignite.AspNetCore.HealthChecks.UI.Hosting;
+using ES.FX.Ignite.OpenTelemetry.Exporter.Seq.Hosting;
+using ES.FX.Ignite.Serilog.Hosting;
+using ES.FX.Ignite.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Seq.Api;
+
+namespace ES.FX.Ignite.OpenTelemetry.Exporter.Seq.Tests
+{
+
+    public class FunctionalTests(SeqContainerFixture seqFixture)
+        : IClassFixture<SeqContainerFixture>
+    {
+        [Fact]
+        public async Task EventsArePresentInSeq()
+        {
+            var builder = WebApplication.CreateBuilder([]);
+
+            builder.Ignite();
+            builder.IgniteHealthChecksUi();
+            builder.IgniteSeqOpenTelemetryExporter(SeqOpenTelemetryExporterSpark.ConfigurationSectionPath, (x) =>
+            {
+                x.Enabled = true;
+            }, (x) =>
+            {
+                x.IngestionEndpoint = seqFixture.GetConnectionString();
+                x.HealthUrl = seqFixture.GetConnectionString() + "/health";
+            });
+
+            builder.IgniteSerilog();
+
+            var app = builder.Build();
+
+            app.Ignite();
+            app.IgniteHealthChecksUi();
+
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+#pragma warning disable xUnit1030 // Do not call ConfigureAwait(false) in test method
+            app.RunAsync().ConfigureAwait(false);
+#pragma warning restore xUnit1030 // Do not call ConfigureAwait(false) in test method
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+
+            // bad approach, but we need to wait for the app to start
+            await Task.Delay(10000);
+
+
+            SeqConnection client = new SeqConnection(seqFixture.GetConnectionString());
+            var events = await client.Events.ListAsync();
+            Assert.NotNull(events);
+            Assert.NotEmpty(events);
+        }
+
+    }
+}
---
+++ tests/ES.FX.Shared.Seq.Tests/Fixtures/SeqContainerFixture.cs
@@ -0,0 +1,32 @@
+﻿using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
+
+namespace ES.FX.Shared.Seq.Tests.Fixtures;
+
+public sealed class SeqContainerFixture : IAsyncLifetime
+{
+    public const string Registry = "datalust";
+    public const string Image = "seq";
+    public const string Tag = "latest";
+    public IContainer? Container { get; private set; }
+
+    public async Task InitializeAsync()
+    {
+        Container = new ContainerBuilder()
+            .WithName($"{nameof(SeqContainerFixture)}-{Guid.NewGuid()}")
+            .WithImage($"{Registry}/{Image}:{Tag}")
+            .WithWaitStrategy(Wait.ForUnixContainer().UntilHttpRequestIsSucceeded(request => request
+                            .ForPath("/")))
+            .WithEnvironment("ACCEPT_EULA", "Y")
+            .WithPortBinding(5341, 80)
+            .Build();
+        await Container.StartAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        if (Container is not null) await Container.DisposeAsync();
+    }
+
+    public string GetConnectionString() => $"http://{Container?.Hostname}:{Container?.GetMappedPublicPort(80)}";
+}
