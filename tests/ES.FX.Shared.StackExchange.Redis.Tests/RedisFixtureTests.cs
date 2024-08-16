14299413f7052934ea7b53492d0025147e0b8c71
Cristian Serban <cristian.serban@selfpay.ro>
2024-08-16 12:06:14 +0300

    add Redis Container fixture

---
+++ tests/ES.FX.Shared.StackExchange.Redis.Tests/RedisFixtureTests.cs
@@ -0,0 +1,42 @@
+using ES.FX.Shared.StackExchange.Redis.Tests.Fixtures;
+using StackExchange.Redis;
+
+namespace ES.FX.Shared.StackExchange.Redis.Tests;
+
+public class RedisFixtureTests(RedisContainerFixture redisContainerFixture)
+    : IClassFixture<RedisContainerFixture>
+{
+    [Fact]
+    public async Task RedisContainer_CanExecuteScript()
+    {
+        const string script = @"
+        -- Lua script
+        for i = 1, 5, 1 do
+          redis.call('incr', 'my-counter')
+        end
+        local mycounter = redis.call('get', 'my-counter')
+        return mycounter
+      ";
+
+        Assert.NotNull(redisContainerFixture.Container);
+        var result = await redisContainerFixture.Container.ExecScriptAsync(script);
+
+        Assert.Equal(0, result.ExitCode);
+        Assert.Contains("5", result.Stdout);
+    }
+
+    [Theory]
+    [InlineData("my-key", "my-value")]
+    public async Task RedisContainer_CanConnect(string key, string value)
+    {
+        Assert.NotNull(redisContainerFixture.Container);
+        var _connectionMultiplexer = await ConnectionMultiplexer.ConnectAsync(redisContainerFixture.Container.GetConnectionString());
+
+        var database = _connectionMultiplexer.GetDatabase();
+        await database.StringSetAsync(key, value);
+
+        var actualValue = await database.StringGetAsync(key);
+        Assert.Equal(actualValue, value);
+
+    }
+}
