29eb73389229967026004e84d29a7651ef79a03f
Serban Cristian <169143793+serban-cristian@users.noreply.github.com>
2024-08-19 11:45:59 +0300

    Add Redis hosting & functional tests (#33)
    * at the begining of each sql functional test it should re-init the container to make sure we start clean
    added more sql unit tests
    
    * add ignite migrations tests
    
    * add nswag tests
    
    * add 1 unit test for health check
    
    * add ignite serilog tests
    
    * add Swashbuckle functional and unit tests
    
    * add seq fixture container and seq functional tests
    
    * more unit tests for sql
    
    * export a random port for seq to avoid conflicts
    
    * create SUT for OpenTelemetry (seq)
    add an option to send the seq connection string from the fixture to the SUT (dynamic ports and so on)
    
    * added unit tests for ES.FX.StackExchange.Redis
    
    * add Redis Container fixture
    
    * add redis hosting and functional tests
    
    * small fix
    
    * Update after recent changes

---
+++ tests/ES.FX.Shared.StackExchange.Redis.Tests/Fixtures/RedisContainerFixture.cs
@@ -0,0 +1,28 @@
+﻿using Testcontainers.Redis;
+
+namespace ES.FX.Shared.StackExchange.Redis.Tests.Fixtures;
+
+public sealed class RedisContainerFixture : IAsyncLifetime
+{
+    public const string Image = "redis";
+    public const string Tag = "latest";
+    public RedisContainer? Container { get; private set; }
+
+    public async Task InitializeAsync()
+    {
+        Container = new RedisBuilder()
+            .WithName($"{nameof(RedisContainerFixture)}-{Guid.NewGuid()}")
+            .WithImage($"{Image}:{Tag}")
+            .Build();
+        await Container.StartAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        if (Container is not null) await Container.DisposeAsync();
+    }
+
+    public string GetConnectionString() =>
+        Container?.GetConnectionString() ??
+        throw new InvalidOperationException("The test container was not initialized.");
+}
