7f6c48ebee3f68cd5e2b3e02f573d5ecca729601
Romeo Dumitrescu <romeo@drc.ro>
2024-09-15 23:27:01 +0300

    feat(MassTransit): add message type observer for consumers
    - Added a new file `MassTransitConsumerMessageTypeObserver.cs` in the `ES.FX.MassTransit/Middleware/MessageTypes` directory.
    - This class observes and registers all expected message types with the `MassTransitMessageTypeProvider`.
    - It implements the `IConsumerConfigurationObserver` interface, which allows it to be notified when a consumer or a consumer's message is configured.

---
+++ src/ES.FX.MassTransit/Middleware/MessageTypes/MassTransitConsumerMessageTypeObserver.cs
@@ -0,0 +1,23 @@
+﻿using ES.FX.MassTransit.Serialization;
+using MassTransit;
+
+namespace ES.FX.MassTransit.Middleware.MessageTypes;
+
+/// <summary>
+///     Observer that registers all expected message types with the <see cref="MassTransitMessageTypeProvider" />
+/// </summary>
+public class MassTransitConsumerMessageTypeObserver : IConsumerConfigurationObserver
+{
+    public void ConsumerConfigured<TConsumer>(IConsumerConfigurator<TConsumer> configurator)
+        where TConsumer : class
+    {
+    }
+
+    public void ConsumerMessageConfigured<TConsumer, TMessage>(
+        IConsumerMessageConfigurator<TConsumer, TMessage> configurator)
+        where TConsumer : class
+        where TMessage : class
+    {
+        MassTransitMessageTypeProvider.RegisterTypes(typeof(TMessage));
+    }
+}
