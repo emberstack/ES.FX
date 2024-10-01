1d85dc6a033cc8af165177a84737508cecf385f6
Romeo Dumitrescu <romeo@drc.ro>
2024-10-01 20:17:48 +0300

    test(ES.FX.Serilog): enhance ProgramEntrySerilogExtensionsTests
    - Moved the assertion inside the RunAsync method to ensure log events are checked during runtime.

--- tests/ES.FX.Serilog.Tests/Lifetime/ProgramEntrySerilogExtensionsTests.cs
+++ tests/ES.FX.Serilog.Tests/Lifetime/ProgramEntrySerilogExtensionsTests.cs
@@ -13,8 +13,11 @@ public class ProgramEntrySerilogExtensionsTests
         await ProgramEntry.CreateBuilder([]).UseSerilog(LogEventLevel.Verbose,
                 config => { config.WriteTo.InMemory(); })
             .Build()
-            .RunAsync(_ => Task.FromResult(1));
+            .RunAsync(_ =>
+            {
+                Assert.NotEmpty(InMemorySink.Instance.LogEvents);
+                return Task.FromResult(1);
+            });
 
-        Assert.NotEmpty(InMemorySink.Instance.LogEvents);
     }
 }
