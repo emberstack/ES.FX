41c8a10047238180a6116de756a9c88a93a41fdb
Romeo Dumitrescu <romeo@drc.ro>
2024-10-08 14:08:58 +0300

    feat(Payloads): add PayloadTypeAttribute for custom payload types
    - Introduced a new attribute, `PayloadTypeAttribute`, to specify a custom type for a payload.
    - This can be used to override the default type or for lookups during serialization/deserialization.
    - Added methods `PayloadTypeFor(Type type)` and `PayloadTypeFor<T>()` to retrieve the custom payload type.

---
+++ src/ES.FX.Contracts/Payloads/PayloadTypeAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Concurrent;
+using System.Reflection;
+using JetBrains.Annotations;
+
+namespace ES.FX.Contracts.Payloads;
+
+/// <summary>
+///     Attribute used to specify a custom type for a payload.
+///     This can be used to override the default type or for lookups during serialization/deserialization
+/// </summary>
+/// <param name="type">The custom type for the payload</param>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface)]
+public class PayloadTypeAttribute(string type) : Attribute
+{
+    private static readonly ConcurrentDictionary<Type, PayloadTypeAttribute?> TypeAttributes = new();
+
+    public string PayloadType { get; } = type;
+
+    public static string? PayloadTypeFor(Type type)
+    {
+        if (TypeAttributes.TryGetValue(type, out var attribute)) return attribute?.PayloadType;
+        attribute = type.GetCustomAttribute<PayloadTypeAttribute>();
+        TypeAttributes.TryAdd(type, attribute);
+        return attribute?.PayloadType;
+    }
+
+    [PublicAPI]
+    public static string? PayloadTypeFor<T>() => PayloadTypeFor(typeof(T));
+}
