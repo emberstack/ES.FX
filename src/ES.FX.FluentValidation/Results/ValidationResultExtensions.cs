f268b40b9c9b02e42fd922e1b7862ab49b317a9e
Romeo Dumitrescu <romeo@drc.ro>
2024-10-08 19:52:04 +0300

    feat(ES.FX.FluentValidation): add JetBrains.Annotations package and ValidationResultExtensions
    - Added a new package reference to JetBrains.Annotations in the ES.FX.FluentValidation.csproj file.
    - Created a new file ValidationResultExtensions.cs with a static method to convert validation results into dictionary format.

---
+++ src/ES.FX.FluentValidation/Results/ValidationResultExtensions.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation.Results;
+using JetBrains.Annotations;
+
+namespace ES.FX.FluentValidation.Results;
+
+public static class ValidationResultExtensions
+{
+    [PublicAPI]
+    public static IDictionary<string, string[]> ToValidationErrors(this ValidationResult validationResult)
+    {
+        return validationResult.Errors
+            .GroupBy(validationFailure => validationFailure.PropertyName)
+            .ToDictionary(
+                validationFailureGrouping => validationFailureGrouping.Key,
+                validationFailureGrouping => validationFailureGrouping
+                    .Select(validationFailure => validationFailure.ErrorMessage).ToArray());
+    }
+}
