5ba5ab093eb8baa81fb581c52a6fc7258d8f001b
Romeo Dumitrescu <romeo@drc.ro>
2024-10-10 00:36:23 +0300

    feat(ES.FX.Problems.AspNetCore): add new AspNetCore project
    - Added a new project "ES.FX.Problems.AspNetCore" to the solution.
    - Created a new .csproj file for the AspNetCore project with target framework as net8.0 and enabled ImplicitUsings and Nullable.
    - Included a reference to Microsoft.AspNetCore.App in the .csproj file.
    - Added a reference to another project "ES.FX.Problems" in the .csproj file.
    - Introduced a new class "ProblemToProblemDetailsExtensions" which includes an extension method to convert Problem objects into ProblemDetails objects.

---
+++ src/ES.FX.Problems.AspNetCore/ProblemToProblemDetailsExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Reflection;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ES.FX.Problems.AspNetCore;
+
+public static class ProblemToProblemDetailsExtensions
+{
+    [PublicAPI]
+    public static ProblemDetails ToProblemDetails(this Problem problem)
+    {
+        var propertiesToSkip = new HashSet<string>
+        {
+            nameof(Problem.Detail),
+            nameof(Problem.Title),
+            nameof(Problem.Type),
+            nameof(Problem.Instance)
+        };
+
+        return new ProblemDetails
+        {
+            Detail = problem.Detail,
+            Status = problem is ValidationProblem
+                ? (int)HttpStatusCode.BadRequest
+                : (int)HttpStatusCode.PreconditionFailed,
+            Title = problem.Title,
+            Type = problem.Type,
+            Instance = problem.Instance,
+            Extensions = problem.GetType()
+                .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                .Where(prop => prop.CanRead && !propertiesToSkip.Contains(prop.Name))
+                .ToDictionary(
+                    prop => prop.Name,
+                    prop => prop.GetValue(problem))
+        };
+    }
+}
