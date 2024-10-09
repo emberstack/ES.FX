9880a7a9c7bec23154c92fe600b470d7045c278d
Romeo Dumitrescu <romeo@drc.ro>
2024-10-09 23:05:07 +0300

    feat(ES.FX.Problems.FluentValidation): add new FluentValidationProblemExtensions and ValidationProblem classes
    - Added a new project ES.FX.Problems.FluentValidation to the solution.
    - Created FluentValidationProblemExtensions class with ToValidationProblem method that converts ValidationResult to ValidationProblem.
    - Created ValidationProblem class which extends Problem, specifically for validation errors.

---
+++ src/ES.FX.Problems.FluentValidation/FluentValidationProblemExtensions.cs
@@ -0,0 +1,12 @@
+﻿using ES.FX.FluentValidation.Results;
+using FluentValidation.Results;
+using JetBrains.Annotations;
+
+namespace ES.FX.Problems.FluentValidation;
+
+public static class FluentValidationProblemExtensions
+{
+    [PublicAPI]
+    public static ValidationProblem ToValidationProblem(this ValidationResult validationResult) =>
+        new(validationResult.ToValidationErrors());
+}
