5aaa3a33cd2690c2b757d603ecab1ce1adaa26fd
Romeo Dumitrescu <romeo@drc.ro>
2024-10-10 14:52:47 +0300

    refactor(ProblemExtensions): improve JSON serialization and error handling
    - Replaced reflection-based property extraction with JSON serialization for ProblemDetails extensions.
    - Added a JsonSerializerOptions instance to enforce camel casing in serialized JSON.
    - Introduced error handling for potential serialization issues, defaulting to an empty dictionary if errors occur.
    - Updated the way status code is determined, now also considering ValidationProblem instances.
    - Detail field of ProblemDetails now falls back on ReasonPhrases.GetReasonPhrase if problem.Detail is null.

--- src/ES.FX.Microsoft.AspNetCore/Problems/ProblemExtensions.cs
+++ src/ES.FX.Microsoft.AspNetCore/Problems/ProblemExtensions.cs
@@ -1,40 +1,65 @@
 ﻿using System.Net;
-using System.Reflection;
+using System.Text.Json;
 using ES.FX.Problems;
 using JetBrains.Annotations;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
 
 namespace ES.FX.Microsoft.AspNetCore.Problems;
 
 public static class ProblemExtensions
 {
+    private static readonly JsonSerializerOptions JsonSerializerOptions = new()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        DictionaryKeyPolicy = JsonNamingPolicy.CamelCase
+    };
+
+    /// <summary>
+    /// These properties are not included in the extensions dictionary of the ProblemDetails object. They are included as top-level properties.
+    /// </summary>
+    private static readonly HashSet<string> PropertiesToSkip = new(StringComparer.OrdinalIgnoreCase)
+    {
+        nameof(Problem.Detail),
+        nameof(Problem.Title),
+        nameof(Problem.Type),
+        nameof(Problem.Instance),
+        nameof(Problem.Status)
+    };
+
     [PublicAPI]
     public static ProblemDetails ToProblemDetails(this Problem problem)
     {
-        var propertiesToSkip = new HashSet<string>
+        var statusCode = problem.Status ?? (problem is ValidationProblem
+            ? (int)HttpStatusCode.BadRequest
+            : (int)HttpStatusCode.PreconditionFailed);
+
+        Dictionary<string, object?> extensions;
+        try
         {
-            nameof(Problem.Detail),
-            nameof(Problem.Title),
-            nameof(Problem.Type),
-            nameof(Problem.Instance),
-            nameof(Problem.Status)
-        };
+            //Serialize and deserialize to ensure that the object is JSON safe. This also sets the correct casing for the keys and properties.
+            var jsonSafeExtensions = JsonSerializer.Deserialize<Dictionary<string, JsonElement>>(
+                JsonSerializer.Serialize(problem as object, JsonSerializerOptions), JsonSerializerOptions);
+            extensions = jsonSafeExtensions?
+                             .Where(kvp => !PropertiesToSkip.Contains(kvp.Key))
+                             .Select(x => new KeyValuePair<string, object?>(x.Key, x.Value))
+                             .ToDictionary(x => x.Key, x => x.Value)
+                         ?? new Dictionary<string, object?>();
+        }
+        catch
+        {
+            extensions = new Dictionary<string, object?>();
+        }
+
 
         return new ProblemDetails
         {
             Type = problem.Type,
-            Detail = problem.Detail,
-            Status = problem.Status ?? (problem is ValidationProblem
-                ? (int)HttpStatusCode.BadRequest
-                : (int)HttpStatusCode.PreconditionFailed),
+            Detail = problem.Detail ?? ReasonPhrases.GetReasonPhrase(statusCode),
+            Status = statusCode,
             Title = problem.Title,
             Instance = problem.Instance,
-            Extensions = problem.GetType()
-                .GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                .Where(prop => prop.CanRead && !propertiesToSkip.Contains(prop.Name))
-                .ToDictionary(
-                    prop => prop.Name,
-                    prop => prop.GetValue(problem))
+            Extensions = extensions
         };
     }
 }
