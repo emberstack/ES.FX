4ada700c5e14c0530dbb7d5179612f8e8a7aa821
Romeo Dumitrescu <romeo@drc.ro>
2025-03-18 08:03:29 +0200

    feat(messaging): add MassTransit RabbitMQ integration
    - Introduced a new project for MassTransit RabbitMQ.
    - Added `RabbitMqOptions` class for configuration settings.
    - Implemented `MessagingServicesExtensions` to register services with dependency injection.
    - Updated MediatR project references and added necessary package dependencies.
    
    BREAKING CHANGE: Refactored namespaces and service registration methods.

---
+++ src/ES.FX.MassTransit/Messaging/MessagingServicesExtensions.cs
@@ -0,0 +1,22 @@
+﻿using ES.FX.Messaging;
+using JetBrains.Annotations;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ES.FX.MassTransit.Messaging;
+
+[PublicAPI]
+public static class MessagingServicesExtensions
+{
+    /// <summary>
+    /// Registers the <see cref="MassTransitMessenger"/> service implementation of <see cref="IMessenger"/>
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection" /> to add services to.</param>
+    /// <param name="lifetime">
+    ///     The lifetime of the <see cref="MassTransitMessenger" />. Default is
+    ///     <see cref="ServiceLifetime.Singleton" />.
+    /// </param>
+    public static void AddMassTransitMessenger(this IServiceCollection services, ServiceLifetime lifetime = ServiceLifetime.Singleton)
+    {
+        services.Add(new ServiceDescriptor(typeof(IMessenger),typeof(MassTransitMessenger), lifetime));
+    }
+}
--- src/ES.FX.MediatR/Messaging/MessengerRelayMessageHandler.cs
+++ src/ES.FX.MediatR/Messaging/MessengerRelayMessageHandler.cs
@@ -2,12 +2,12 @@
 using ES.FX.Messaging;
 using MediatR;
 
-namespace ES.FX.MediatR;
+namespace ES.FX.MediatR.Messaging;
 /// <summary>
 /// Represents a <see cref="IRequestHandler{RelayMessage}"/> that sends the <see cref="IMessage"/> to the <see cref="IMessenger"/> supplied
 /// </summary>
 /// <param name="messenger">The <see cref="IMessenger"/> used to send the <see cref="IMessage"/></param>
-public class MessengerRelayMessageRequestHandler(IMessenger messenger):IRequestHandler<RelayMessage>
+public class MessengerRelayMessageHandler(IMessenger messenger):IRequestHandler<RelayMessage>
 {
     public async Task Handle(RelayMessage request, CancellationToken cancellationToken)
     {
