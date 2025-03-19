4af83b8ae03d347fba2af3fc3246e5216152e0ea
Romeo Dumitrescu <romeo@drc.ro>
2025-03-19 13:39:14 +0200

    feat(identity): add Microsoft.Extensions.Identity.Core package
    - Introduced a new project for Microsoft.Extensions.Identity.Core.
    - Implemented a GenericPasswordHasher class for password hashing functionality.
    - Updated package versions in Directory.Packages.props to ensure compatibility.
    
    BREAKING CHANGE: Updated several dependencies to newer versions.

---
+++ src/ES.FX.Microsoft.Extensions.Identity.Core/Shared/GenericPasswordHasher.cs
@@ -0,0 +1,31 @@
+﻿using JetBrains.Annotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace ES.FX.Microsoft.Extensions.Identity.Core.Shared;
+
+/// <summary>
+///     A generic (non-user dependent) password hasher based on <see cref="IPasswordHasher{TUser}" />
+/// </summary>
+[PublicAPI]
+public class GenericPasswordHasher
+{
+    private static readonly object User = new();
+    private readonly PasswordHasher<object> _hasher = new();
+
+    public static GenericPasswordHasher Instance { get; } = new();
+
+    /// <summary>
+    ///     Returns a hashed representation of the supplied <paramref name="password" />.
+    /// </summary>
+    public string HashPassword(string password) => _hasher.HashPassword(User, password);
+
+    /// <summary>
+    ///     Returns a <see cref="PasswordVerificationResult" /> indicating the result of a password hash comparison.
+    /// </summary>
+    /// <param name="hashedPassword">The hash value for the password.</param>
+    /// <param name="providedPassword">The password supplied for comparison.</param>
+    /// <returns>A <see cref="PasswordVerificationResult" /> indicating the result of a password hash comparison.</returns>
+    /// <remarks>Implementations of this method should be time consistent.</remarks>
+    public PasswordVerificationResult VerifyHashedPassword(string hashedPassword, string providedPassword) =>
+        _hasher.VerifyHashedPassword(User, hashedPassword, providedPassword);
+}
