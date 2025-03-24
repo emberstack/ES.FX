26c1cc798a2caf906fc70957a1e1281b351d62ec
Romeo Dumitrescu <romeo@drc.ro>
2025-03-24 12:29:24 +0200

    docs(JsonSerializerExtensions): enhance XML documentation for deserialization methods
    - Improved XML comments for `TryDeserialize<T>` methods to clarify parameters and return values.
    - Added detailed descriptions of behavior, including exception handling during deserialization.
    - Ensured consistency in documentation style across both overloads of the method.

--- src/ES.FX.Extensions.System.Text.Json/Serialization/JsonSerializerExtensions.cs
+++ src/ES.FX.Extensions.System.Text.Json/Serialization/JsonSerializerExtensions.cs
@@ -4,17 +4,36 @@ using JetBrains.Annotations;
 
 namespace ES.FX.Extensions.System.Text.Json.Serialization;
 
+/// <summary>
+///     Provides extension methods for JSON deserialization using <see cref="JsonSerializer" />.
+/// </summary>
 [PublicAPI]
 public static class JsonSerializerExtensions
 {
     /// <summary>
-    ///     <inheritdoc cref="JsonSerializer.Deserialize{TValue}(string,System.Text.Json.JsonSerializerOptions?)" />
+    ///     Attempts to deserialize the specified JSON string into an instance of type <typeparamref name="T" />.
     /// </summary>
+    /// <typeparam name="T">The type into which to deserialize the JSON.</typeparam>
+    /// <param name="json">A string containing JSON data.</param>
+    /// <param name="result">
+    ///     When this method returns, contains the deserialized object of type <typeparamref name="T" />,
+    ///     if the deserialization succeeded, or the default value of <typeparamref name="T" /> if it failed.
+    /// </param>
+    /// <param name="options">
+    ///     The <see cref="JsonSerializerOptions" /> to use during deserialization. If <c>null</c>,
+    ///     <see cref="JsonSerializerOptions.Web" /> is used.
+    /// </param>
+    /// <returns>
+    ///     <c>true</c> if the JSON string was successfully deserialized into an instance of <typeparamref name="T" />;
+    ///     otherwise, <c>false</c>.
+    /// </returns>
     /// <remarks>
-    ///     This uses <see cref="JsonSerializerOptions.Web" /> by if no value is supplied for
-    ///     <param name="options" />
+    ///     This method catches any exceptions thrown during deserialization and returns <c>false</c> in such cases.
     /// </remarks>
-    public static bool TryDeserialize<T>([StringSyntax(StringSyntaxAttribute.Json)] this string json, out T? result,
+    public static bool TryDeserialize<T>(
+        [StringSyntax(StringSyntaxAttribute.Json)]
+        this string json,
+        out T? result,
         JsonSerializerOptions? options = null)
     {
         try
@@ -30,13 +49,29 @@ public static class JsonSerializerExtensions
     }
 
     /// <summary>
-    ///     <inheritdoc cref="JsonSerializer.Deserialize{TValue}(System.IO.Stream,System.Text.Json.JsonSerializerOptions?)" />
+    ///     Attempts to deserialize the JSON data from the specified UTF-8 encoded stream into an instance of type
+    ///     <typeparamref name="T" />.
     /// </summary>
+    /// <typeparam name="T">The type into which to deserialize the JSON.</typeparam>
+    /// <param name="utf8Json">A stream containing UTF-8 encoded JSON data.</param>
+    /// <param name="result">
+    ///     When this method returns, contains the deserialized object of type <typeparamref name="T" />,
+    ///     if the deserialization succeeded, or the default value of <typeparamref name="T" /> if it failed.
+    /// </param>
+    /// <param name="options">
+    ///     The <see cref="JsonSerializerOptions" /> to use during deserialization. If <c>null</c>,
+    ///     <see cref="JsonSerializerOptions.Web" /> is used.
+    /// </param>
+    /// <returns>
+    ///     <c>true</c> if the JSON stream was successfully deserialized into an instance of <typeparamref name="T" />;
+    ///     otherwise, <c>false</c>.
+    /// </returns>
     /// <remarks>
-    ///     This uses <see cref="JsonSerializerOptions.Web" /> by if no value is supplied for
-    ///     <param name="options" />
+    ///     This method catches any exceptions thrown during deserialization and returns <c>false</c> in such cases.
     /// </remarks>
-    public static bool TryDeserialize<T>(this Stream utf8Json, [NotNullWhen(true)] out T? result,
+    public static bool TryDeserialize<T>(
+        this Stream utf8Json,
+        [NotNullWhen(true)] out T? result,
         JsonSerializerOptions? options = null)
     {
         try
