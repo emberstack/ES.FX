3f8fb0e102b31f6bcfafbf4ede28cc97d5b5cef0
Romeo Dumitrescu <romeo@drc.ro>
2025-03-24 17:13:19 +0200

    refactor(json): rename UnixTimeDateTimeOffset converter classes
    - Renamed `UnixTimeDateTimeOffsetJsonConverter` to `UnixTimeMillisecondsDateTimeOffsetJsonConverter` for clarity.
    - Updated the read and write methods to handle milliseconds instead of seconds.
    - Changed behavior to throw a `JsonException` on null or empty string inputs instead of returning null.
    - Created additional converters for Unix time in seconds with nullable support.
    
    BREAKING CHANGE: The converters now require non-nullable DateTimeOffset types.

---
+++ src/ES.FX.Extensions.System.Text.Json/Serialization/Converters/UnixTimeSecondsDateTimeOffsetJsonConverter.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using JetBrains.Annotations;
+
+namespace ES.FX.Extensions.System.Text.Json.Serialization.Converters;
+
+/// <summary>
+///     Converts Unix time values to and from nullable <see cref="DateTimeOffset" /> objects.
+/// </summary>
+/// <remarks>
+///     Unix time is defined as the number of seconds that have elapsed since 00:00:00 UTC on 1 January 1970 (the Unix
+///     epoch).
+///     This converter supports Unix time values represented either as JSON numbers or as strings.
+///     When encountering a JSON null or an empty string, the converter throws a <see cref="JsonException"/>
+/// </remarks>
+[PublicAPI]
+public class UnixTimeSecondsDateTimeOffsetJsonConverter : JsonConverter<DateTimeOffset>
+{
+    public override DateTimeOffset Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        switch (reader)
+        {
+            
+            case { TokenType: JsonTokenType.String } :
+            {
+                var stringValue = reader.GetString();
+                if (string.IsNullOrEmpty(stringValue)) 
+                    throw new JsonException("Invalid token type for Unix epoch time");
+                if (!long.TryParse(stringValue, out var unixTime))
+                    throw new JsonException($"Invalid Unix epoch time string: {stringValue}");
+                return DateTimeOffset.FromUnixTimeSeconds(unixTime);
+            }
+            case { TokenType: JsonTokenType.Number }:
+            {
+                var unixTime = reader.GetInt64();
+                return DateTimeOffset.FromUnixTimeSeconds(unixTime);
+            }
+            default:
+                throw new JsonException("Invalid token type for Unix epoch time");
+        }
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateTimeOffset value, JsonSerializerOptions options)
+    {
+        writer.WriteNumberValue(value.ToUnixTimeSeconds());
+    }
+}
